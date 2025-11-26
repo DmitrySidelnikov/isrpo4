683a3cca5d3109bc920cbe04c0819821f68ae217
Dima <fulikaslxner@gmail.com>
2025-11-26 22:01:54 +0300

    circle.py

---
+++ circle.py
@@ -0,0 +1,12 @@
+import math
+def area(r):
+'''
+На вход принимаем число r, возвращаем r^2*pi
+'''
+    return math.pi * r * r
+def perimeter(r):
+'''
+На вход принимаем число r, возвращаем r*2*pi
+'''
+    return 2 * math.pi * r
+
