db74a1dc3cee9cc9329f9a5023e486f868aa0150
Dima <fulikaslxner@gmail.com>
2025-11-27 17:18:00 +0300

    new

--- triangle.py
+++ triangle.py
@@ -1,10 +1,4 @@
 def area(a, h): 
-'''
-    На вход принимаем числа a и h, возвращаем a*h/2
-'''
     return a * h / 2 
 def perimeter(a, b, c):
-'''
-    На вход принимаем числа a, b и c, возвращаем a + b + c
-'''
     return a + b + c 
